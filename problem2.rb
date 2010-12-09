02bb82adcdbf39847f917cf8a8209f8ee2af0f56
Anna Cruz <anna.cruz@gmail.com>
2010-12-09 10:52:29 -0200

    New solution for problem2

--- problem2.rb
+++ problem2.rb
@@ -1,24 +1,11 @@
 #!/usr/bin/ruby
 
-def fib(n)
-	return n if n<2
-	fib(n-1) + fib(n-2)
+n = m = 1
+sum = 0
+
+while m < 4_000_000
+  n,m = m,n+m
+  sum += n if n.even?
 end
 
-def soma(arg)
-	soma(arg) + soma(arg-i1)
-end
-array = []
-total = 0
-#UNTIL ???????
-#while total < 100 do 
-	for i in (1..10) do
-		#array << fib(i)
-		puts fib(i)
-	end
-	#total = array.inject(:+) - 1
-#end
-
-
-#puts array
-#puts total
+puts sum
